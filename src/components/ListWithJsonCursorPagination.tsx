1a1a3f0046f0b3eb20f93ebdf0d9ad5e3ebfd32c
HerbertLiskin <gkljatsko@gmail.com>
2021-08-31 12:48:26 +0300

    fix list end in json cursor

--- src/components/ListWithJsonCursorPagination.tsx
+++ src/components/ListWithJsonCursorPagination.tsx
@@ -7,15 +7,12 @@ import {
 import { UsersListProps } from '../types'
 
 const OFFSET: number = 40;
-const START_PAGE: number = 1
-
 
 const ListWithJsonCursorPagination: React.FC<UsersListProps> = ({usersList}) => {
 	const [usersFullList] = useState<UserItem[]>(usersList)
 	const [startCursor, setStartCursor] = useState<String>(usersFullList[0].id)
 	const [endCursor, setEndCursor] = useState<String>(usersFullList[OFFSET].id)
 	const [usersCurrentList, setUsersCurrentList] = useState<UserItem[]>(usersFullList.slice(0, OFFSET))
-	const [page, setPage] = useState<number>(START_PAGE)
 
 
 	const nextPage = () => {
@@ -25,7 +22,6 @@ const ListWithJsonCursorPagination: React.FC<UsersListProps> = ({usersList}) => 
 		setUsersCurrentList(currentList)
 		setStartCursor(currentList[0].id)
 		setEndCursor(currentList[currentList.length -1].id)
-		setPage(page + 1)
 	}
 
 	const prevPage = () => {
@@ -38,7 +34,6 @@ const ListWithJsonCursorPagination: React.FC<UsersListProps> = ({usersList}) => 
 		setUsersCurrentList(currentList)
 		setStartCursor(currentList[0].id)
 		setEndCursor(currentList[currentList.length -1].id)
-		setPage(page - 1)
 	}
 
 	const handleScrole = (e: WheelEvent<HTMLDivElement>) => {
@@ -52,13 +47,14 @@ const ListWithJsonCursorPagination: React.FC<UsersListProps> = ({usersList}) => 
 
 		if(
 			isScrollDown && 
-			scrollTop >= wrapperHeight - overflowHeight*2
+			scrollTop >= wrapperHeight - overflowHeight*2 &&
+			usersCurrentList[usersCurrentList.length - 1].index < usersFullList[usersFullList.length - 1].index
 		) {
 			nextPage()
 		} else if(
 			!isScrollDown && 
 			scrollTop <= overflowHeight*2 && 
-			page > 1
+			usersCurrentList[0].index > 0
 		) {
 			prevPage()
 		}
@@ -72,7 +68,7 @@ const ListWithJsonCursorPagination: React.FC<UsersListProps> = ({usersList}) => 
 			<div className='text-white text-3xl mb-3'>List with somthing like cursor pagination</div>
 			<div className='text-white text-xl mb-3'>Previous/next elements remove on scroll. Cursor bound to id's.</div>
 			<div className='w-full flex justify-between'>
-				<div className='mb-2 p-2 bg-red-500 rounded-md text-white'>Page №: {page}</div>
+				<div className='mb-2 p-2 bg-red-500 rounded-md text-white'>Total count: {usersFullList.length}</div>
 				<div className='mb-2 p-2 bg-red-500 rounded-md text-white'>Elements [start - end] indexes: {listFirstIndex} - {listLastIndex}</div>
 			</div>
 				<div 
